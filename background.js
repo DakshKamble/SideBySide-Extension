8a84f4b8e190758b64d90ada72e0b4c2d6e8e035
DakshKamble <dakshdk08@gmail.com>
2026-04-27 15:09:35 +0530

    add: gitignore

--- background.js
+++ background.js
@@ -1,16 +1,13 @@
 //When msg is recieved call this function
-chrome.runtime.onMessage.addListener(createRoom_server);
-chrome.runtime.onMessage.addListener(joinRoom_server);
+chrome.runtime.onMessage.addListener(roomHandler);
 
-function createRoom_server(message) {
+//This function manages connection with server
+function roomHandler(message) {
     //only activate if create-room command sent 
     if(message["room-action"] == "create-room") {
         console.log("creating room");
-    }
-}
 
-function joinRoom_server(message) {
-    if(message["room-action"] == "join-room") {
+    } else if(message["room-action"] == "join-room") {
         console.log("Joining room");
     }
 }
