b834fcee50a3cc8fb4c5803e9bf35ca387834081
DakshKamble <dakshdk08@gmail.com>
2026-04-29 11:48:53 +0530

    add: working room system

--- background.js
+++ background.js
@@ -1,13 +1,37 @@
 //When msg is recieved call this function
 chrome.runtime.onMessage.addListener(roomHandler);
 
+const socket = new WebSocket("ws://localhost:8888"); //conect to teh websocket
 //This function manages connection with server
+
+//handle server responses
+socket.addEventListener('message', (event) => {
+    const data = JSON.parse(event.data); //parse to be abel to read
+    
+    if (data["server-action"] == "new-room-code") {
+        socket.send(JSON.stringify({"server-action" : "join-room", roomId: data.roomId}));
+    }
+
+    if(data["auth"] == "success") {
+        chrome.runtime.sendMessage({"background-action" : "connected", roomId : data.roomId});
+    }
+
+    if(data["auth"] == "failed") {
+        chrome.runtime.sendMessage({"background-action" : "failed"});
+    }
+});
+
+//send to server
 function roomHandler(message) {
     //only activate if create-room command sent 
-    if(message["room-action"] == "create-room") {
+    if(message["popup-action"] == "create-room") {
         console.log("creating room");
 
-    } else if(message["room-action"] == "join-room") {
+        socket.send(JSON.stringify({"server-action" : "new-room-code"})) //ask server for roomcode new
+
+    } else if(message["popup-action"] == "join-room") {
         console.log("Joining room");
+        socket.send(JSON.stringify({"server-action" : "join-room", roomId: message.roomId})); // send connect request to server
+
     }
 }
--- popup.js
+++ popup.js
@@ -9,15 +9,18 @@ const btnJoinRoom = document.getElementById("btn-join-room");
 
 const inputRoomCode = document.getElementById("input-room-code");
 
+const connectStatus = document.getElementById("connect-status");
+
+const roomId = document.getElementById("room-id");
+
 //Adding event listener 'onclick' to the buttons
 btnCreateRoom.addEventListener('click', createRoom);
 btnJoinRoom.addEventListener('click', joinRoom);
 
-
 //This funcion contacs the service and tells content.js to open the side panel
 function createRoom() {
     console.log("Creating Room!");
-    chrome.runtime.sendMessage({"room-action" : "create-room"});
+    chrome.runtime.sendMessage({"popup-action" : "create-room"});
 }
 
 function joinRoom() {
@@ -25,6 +28,21 @@ function joinRoom() {
         console.log("Please enter valid room code");
     } else {
         console.log("Joining Room!");
-        chrome.runtime.sendMessage({"room-action" : "join-room"});
+        chrome.runtime.sendMessage({"popup-action" : "join-room", roomId: inputRoomCode.value}); //this sends the room-action to background.js along with room code
+    }
+}
+
+chrome.runtime.onMessage.addListener(background_Handler);
+
+
+function background_Handler(message) {
+    if(message["background-action"] == "connected") {
+        connectStatus.innerText = "Connected";
+        roomId.innerText = message.roomId;
+        
+    }
+
+    if(message["background-action"] == "failed") {
+        connectStatus.innerText = "invalid";
     }
 }
